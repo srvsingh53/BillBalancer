6e8df76d27e4d357a82a501614fadda3ec35de92
Saurabh Singh <139792671+srvsingh53@users.noreply.github.com>
2023-08-08 03:16:18 +0530

    Update billspit.py

--- billspit.py
+++ billspit.py
@@ -2,43 +2,48 @@ def input_names(name, n):
     for i in range(n):
         print("Enter the name: ", end="")
         d = input() # string
-        name[d] = i # string to
+        name[d] = i # assign a label to the name in the dictionary
         print()
 
 def find_label(name, n, d):
-    return name[d]
+    return name[d] # returns the label associated with the name
 
 def find_name(m, n, value):
     for key, val in m.items():
         if val == value:
-            return key
+            return key # finds the name associated with a label
 
 def main():
     n = int(input("Enter the number of persons: "))
-    name = {}
-    input_names(name, n)# input is a pair of name and its label in adj matrix
+    name = {} # dictionary to store names and their labels
+    input_names(name, n) # input names and assign labels
+
+    # Create an empty 2D list (matrix) to track expenses between individuals
     arr = [[0 for _ in range(n)] for _ in range(n)]
 
+    # Loop to input expenses and update the expense matrix
     while True:
         m = int(input("What's the expense? : "))
         p = input("WHO PAID IT? : ")
-        x = find_label(name, n, p)
-        expense = m // n
+        x = find_label(name, n, p) # find the label for the person who paid
+        expense = m // n # calculate the equal expense per person
 
         for i in range(n):
             if i == x:
                 arr[x][x] = 0
             else:
-                arr[x][i] += expense
+                arr[x][i] += expense # update the expense matrix
 
         test = int(input("Press -1 to stop OR press any number to continue: "))
         if test == -1:
             break
 
+    # Print the expense matrix
     print("\n")
     for row in arr:
         print(" ".join(map(str, row)))
 
+    # Calculate and print who owes whom
     print("\n")
     print("\n")
     print("\n")
